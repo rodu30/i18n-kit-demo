a0f340697743ccc2407e3fdbd468ab3e2e2c07ab
Roman Duhr <roman.duhr@gmail.com>
2017-11-20 18:40:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { formatNumber, formatDateTime } from 'i18n-kit';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locale: 'en-US'
+    };
+  }
+
+  handleChangeToDE = () => {
+    this.setState({ locale: 'de-DE' });
+  };
+
+  handleChangeToEN = () => {
+    this.setState({ locale: 'en-US' });
+  };
+
+  render() {
+    // TODO: global festlegen
+    const { locale } = this.state;
+
+    const number = formatNumber(locale, 3500);
+    const money = formatNumber(locale, 29.99, { style: 'currency', currency: 'USD' });
+
+    const now = formatDateTime(locale, new Date());
+    const nowInPerth = formatDateTime('locale', new Date(), {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZone: 'Australia/Perth',
+      timeZoneName: 'short'
+    });
+    const nowWithTime = formatDateTime('locale', new Date(), {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      weekday: 'long'
+    });
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button
+          onClick={this.handleChangeToDE}
+          style={{
+            background: locale === 'de-DE' && 'blue',
+            color: locale === 'de-DE' && '#fff'
+          }}
+        >
+          DE
+        </button>
+        <button
+          onClick={this.handleChangeToEN}
+          style={{
+            background: locale === 'en-US' && 'blue',
+            color: locale === 'en-US' && '#fff'
+          }}
+        >
+          EN
+        </button>
+        <h3>Numbers</h3>
+        <p>{number}</p>
+        <p>{money}</p>
+        <h3>DateTime</h3>
+        <p>{now}</p>
+        <p>{nowWithTime}</p>
+        <p>{nowInPerth}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
