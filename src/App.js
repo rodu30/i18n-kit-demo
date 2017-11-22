706186e92a38ca399b3bedeeff2d9691f8cdde02
Roman Duhr <roman.duhr@gmail.com>
2017-11-22 01:05:21 +0100

    message translation added to examples

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { formatNumber, formatDateTime, formatMessage, generateKeyFromString } from 'i18n-kit';
 import logo from './logo.svg';
-import { formatNumber, formatDateTime } from 'i18n-kit';
 import './App.css';
 
 class App extends Component {
@@ -27,14 +27,14 @@ class App extends Component {
     const money = formatNumber(locale, 29.99, { style: 'currency', currency: 'USD' });
 
     const now = formatDateTime(locale, new Date());
-    const nowInPerth = formatDateTime('locale', new Date(), {
+    const nowInPerth = formatDateTime(locale, new Date(), {
       hour: 'numeric',
       minute: 'numeric',
       second: 'numeric',
       timeZone: 'Australia/Perth',
       timeZoneName: 'short'
     });
-    const nowWithTime = formatDateTime('locale', new Date(), {
+    const nowWithTime = formatDateTime(locale, new Date(), {
       year: 'numeric',
       month: 'numeric',
       day: 'numeric',
@@ -42,6 +42,22 @@ class App extends Component {
       minute: 'numeric',
       second: 'numeric',
       weekday: 'long'
+    });
+
+    const defaultMessage = 'This is {num1} test for {num2}.';
+    const messageKey = generateKeyFromString(defaultMessage);
+    const messages = {
+      'de-DE': { [messageKey]: 'Dies ist {num1} ein Test für {num2}.' },
+      'en-US': { [messageKey]: 'This is {num1} test for {num2}.' }
+    };
+    const testText = formatMessage(locale, messages, 'This is {num1} test for {num2}.', {
+      vars: {
+        num1: 1,
+        num2: 2
+      }
+    });
+    const failingTestText = formatMessage(locale, messages, 'This is another test.', {
+      defaultLocale: 'en-US'
     });
 
     return (
@@ -78,6 +94,9 @@ class App extends Component {
         <p>{now}</p>
         <p>{nowWithTime}</p>
         <p>{nowInPerth}</p>
+        <h3>Text</h3>
+        <p>{testText}</p>
+        <p>{failingTestText}</p>
       </div>
     );
   }
